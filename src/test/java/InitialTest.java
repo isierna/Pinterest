fd9517c3ce3b7f9a1c7756dd2e64869e52a32625
Ira <trys2015@gmail.com>
2017-07-28 09:23:57 +0200

    new

--- src/test/java/InitialTest.java
+++ src/test/java/InitialTest.java
@@ -1,9 +1,7 @@
-import com.sun.tools.javac.comp.Todo;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -38,7 +36,7 @@ public class InitialTest {
     @Test
     public void login() {
         WebDriverWait wait = new WebDriverWait(driver, 20);
-        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@data-reactid=12]/button")));
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='lightGrey active']")));
 
         System.out.println(loginButton.getText());
         loginButton.getText().contains("Log in");
@@ -63,6 +61,36 @@ public class InitialTest {
         System.out.println();
 //        todo: add login button
 
+    }
+
+    @Test
+    public void registration() {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='lightGrey active']")));
+
+        System.out.println(loginButton.getText());
+        loginButton.getText().contains("Log in");
+        loginButton.click();
+
+        WebElement signUpLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='FullPageSignup__signupToggle']")));
+        signUpLink.click();
+
+        WebElement loginWindow = driver.findElement(By.xpath("//div[@class='DenzelReactBridge Module']"));
+        loginWindow.getText().contains("Sign up to see more");
+        Assert.assertTrue(loginWindow.getText().contains("Sign up to see more"));
+
+        WebElement emailInput = driver.findElement(By.xpath("//fieldset[1]"));
+        WebElement passwordInput = driver.findElement(By.xpath("//fieldset[2][@data-reactid=9]"));
+
+        RandomStringUtils randomObject = new RandomStringUtils();
+        String random = randomObject.randomNumeric(3);
+        String userEmail = "trys2015+" + random + "@gmail.com";
+
+        emailInput.sendKeys(userEmail);
+        passwordInput.click();
+        passwordInput.sendKeys("123456zz");
+
+        System.out.println();
     }
 
     @AfterMethod
